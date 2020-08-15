ea21421790f425c339800e44b35a3413d9f942f8
WilkoV <lichtzeichenanlage@googlemail.com>
2020-08-16 00:13:35 +0200

    Deliverables are now created in the directory deliverables and the tool is creating a log file

--- tools/createExportItDeliverable.bash
+++ tools/createExportItDeliverable.bash
@@ -1,5 +1,6 @@
-cd ..
-tar --exclude "__pycache__/*" --exclude "apper/apper/__pycache__/*" --exclude "commands/__pycache__/*" --exclude "tools" --exclude=".git" --exclude=".vscode" --exclude="__pycache__" --exclude="tests" --exclude=".gitignore" --exclude=".gitmodules" --exclude="commands/__pachace__" --exclude="commands/config" --exclude="commands/logs" -zcvf ./tools/ExportIt.tar.gz .
+(cd ..
 
-zip -r ./tools/ExportIt.zip . -x "__pycache__/*" -x "apper/apper/__pycache__/*" -x "commands/__pycache__/*" -x "tools/*" -x ".git/*" -x ".vscode/*" -x "__pycache__/*" -x "tests/*" -x ".gitignore" -x ".gitmodules" -x "commands/__pachace__/*" -x "commands/config/*" -x "commands/logs/*" 
+tar --exclude "__pycache__/*" --exclude "apper/apper/__pycache__/*" --exclude "commands/__pycache__/*" --exclude "tools" --exclude=".git" --exclude=".vscode" --exclude="__pycache__" --exclude="tests" --exclude=".gitignore" --exclude=".gitmodules" --exclude="commands/__pachace__" --exclude="commands/config" --exclude="bin" --exclude="commands/logs" -zcvf ./deliverables/ExportIt.tar.gz .
 
+zip -r ./deliverables/ExportIt.zip . -x "__pycache__/*" -x "apper/apper/__pycache__/*" -x "commands/__pycache__/*" -x "tools/*" -x "bin/*" -x ".git/*" -x ".vscode/*" -x "__pycache__/*" -x "tests/*" -x ".gitignore" -x ".gitmodules" -x "commands/__pachace__/*" -x "commands/config/*" -x "commands/logs/*" 
+) | tee deliverable.log
