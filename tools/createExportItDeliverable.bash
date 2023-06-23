daea37c0f7c6fd67dd077c3d4a1e97f9ebaf6804
WilkoV <lichtzeichenanlage@googlemail.com>
2023-06-23 04:07:55 +0200

    #40

--- tools/createExportItDeliverable.bash
+++ tools/createExportItDeliverable.bash
@@ -4,9 +4,7 @@ rm ./deliverables/ExportIt.tar.gz
 rm ./deliverables/ExportIt.zip
 rm ../commands/logs/deliverable.log
 
-tar --exclude "__pycache__/*" --exclude "apper/apper/__pycache__/*" --exclude "commands/__pycache__/*" --exclude "tools" --exclude=".git" --exclude=".vscode" --exclude="__pycache__" --exclude="tests" --exclude=".gitignore" --exclude=".gitmodules" --exclude="commands/__pachace__" --exclude="commands/config" --exclude="bin" --exclude="commands/logs" --exclude "deliverables/*" -zcvf ./deliverables/ExportIt.tar.gz .
+tar --exclude "__pycache__/*" --exclude "./apper/apper/__pycache__/*" --exclude "./commands/__pycache__/*" --exclude "./tools" --exclude="./.git" --exclude="./.vscode" --exclude="./__pycache__" --exclude="./tests" --exclude="./.gitignore" --exclude="./.gitmodules" --exclude="./commands/__pachace__" --exclude="./commands/config" --exclude="./bin" --exclude="./commands/logs" --exclude "./deliverables" -zcvf ./deliverables/ExportIt.tar.gz .
 
-sleep 2
-
-zip -r ./deliverables/ExportIt.zip . -x "__pycache__" -x "apper/apper/__pycache__" -x "commands/__pycache__" -x "tools" -x ".git" -x ".vscode" -x "__pycache__" -x "tests" -x ".gitignore" -x ".gitmodules" -x "commands/__pachace__" -x "commands/config" -x "bin" -x "commands/logs" -x "deliverables"
+zip -r ./deliverables/ExportIt.zip . -x ./__pycache__/\* -x ./apper/apper/__pycache__/\* -x ./commands/__pycache__/\* -x ./tools/\* -x ./.git/\* -x ./.vscode/\* -x ./__pycache__/\* -x ./tests/\* -x ./.gitignore/\* -x ./.gitmodules/\* -x ./commands/__pachace__/\* -x ./commands/config/\* -x ./bin/\* -x ./commands/logs/\* -x ./deliverables
 ) | tee ../commands/logs/deliverable.log
